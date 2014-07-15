4f932000cbfe624ff8b99774fd7ba7ee07554dc1
root <root@cluster.hpc.org>
2014-07-15 15:25:48 -0700

    Tracing Tier Write

--- trace-write.sh
+++ trace-write.sh
@@ -1,3 +1,3 @@
-trace-cmd record -p function_graph -O nosleep-time -O nofuncgraph-overhead -O graph-time  -l :mod:zfs dd if=/dev/zero of=/tank bs=1K count=1
+trace-cmd record -p function_graph -O nosleep-time -O nofuncgraph-overhead -O graph-time  -l :mod:zfs dd if=/dev/zero of=/tank/testfile bs=1K count=1
 
 #trace-cmd report
