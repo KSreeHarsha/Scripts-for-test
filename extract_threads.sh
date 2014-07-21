657645930e05f9d489e5bede788675d406104304
root <root@cluster.hpc.org>
2014-07-21 13:52:35 -0700

    Scrpt for extracting threads

---
+++ extract_threads.sh
@@ -0,0 +1,17 @@
+filename="scrub_tier.c"
+
+cat $filename |awk '{print $1}' | sort -u
+
+
+
+
+fname=${filename:0:-2}
+
+mkdir $fname
+cat $filename |awk '{gsub("/","",$1); print $1}' | sort -u > ${fname}/threads.c
+
+
+while read line; do cat $filename | grep $line > ${fname}/$line ; done < $fname/threads.c
+
+
+
