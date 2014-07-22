2c7dc4902e9de14eb3c1f080ea32aaa38758bf97
root <root@cluster.hpc.org>
2014-07-22 15:56:07 -0700

    removed-checksum-errors-by-selecting-only-one-child

--- trace_write.sh
+++ trace_write.sh
@@ -1,3 +1,3 @@
-trace-cmd record -p function_graph -O nosleep-time -O nofuncgraph-overhead -O graph-time  -l :mod:zfs dd if=/dev/zero of=/tank/testfile bs=1K count=1 oflag=dsync
+trace-cmd record -p function_graph -O nosleep-time -O nofuncgraph-overhead -O graph-time  -l :mod:zfs dd if=/dev/zero of=/tank/testfile bs=1K count=10 oflag=dsync
 
 #trace-cmd report
